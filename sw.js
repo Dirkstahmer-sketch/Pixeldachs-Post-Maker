dd1a2ba08f81397ee58741797dbe69af0bb1e260
Dirkstahmer-sketch <Dirkstahmer@web.de>
2025-10-02 21:26:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('pixeldachs-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './icon.png'
-    ]))
-  );
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('pixeldachs-v3').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.png','./logo.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
